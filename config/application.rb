f655ce3e1dc13d10fc29957019e54d71b02dccf6
HEMANG SHARMA <manavasharma98@gmail.com>
2021-06-15 11:45:11 -0400

    homepage creation

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Sassapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.hosts << "port-3000-devtool-cebehe1310787554.preview.codeanywhere.com"
 
     # Configuration for the application, engines, and railties goes here.
     #
